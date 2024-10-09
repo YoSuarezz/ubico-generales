75f9dbf944eb4c180182497df2821b60db40a62f
Sebastian <sebassuarez103@gmail.com>
2024-10-08 20:19:03 -0500

    Initial commit

---
+++ src/main/java/co/edu/uco/ubico/generales/domain/city/exceptions/CityIdDoesNotExistsException.java
@@ -0,0 +1,5 @@
+package co.edu.uco.ubico.generales.domain.city.exceptions;
+
+public final class CityIdDoesNotExistsException extends RuntimeException{
+    private static final long serialVersionUTD = 1L;
+}
---
+++ src/main/java/co/edu/uco/ubico/generales/domain/city/rules/CityIdDoesExistsRule.java
@@ -0,0 +1,9 @@
+package co.edu.uco.ubico.generales.domain.city.rules;
+
+import co.edu.uco.ubico.generales.domain.DomainRule;
+
+import java.util.UUID;
+
+public interface CityIdDoesExistsRule extends DomainRule<UUID> {
+
+}
